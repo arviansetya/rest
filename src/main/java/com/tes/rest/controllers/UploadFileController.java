0e6170920fbc950885aa5b6191dc08ce28de8df2
arviansetya <arviansetya04@gmail.com>
2023-02-11 10:46:59 +0700

    add responseEntity

--- src/main/java/com/tes/rest/controllers/UploadFileController.java
+++ src/main/java/com/tes/rest/controllers/UploadFileController.java
@@ -1,6 +1,7 @@
 package com.tes.rest.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +13,20 @@ import com.tes.rest.services.UploadFileService;
 @RestController
 @RequestMapping("/api")
 public class UploadFileController {
-    
+
     @Autowired
     private UploadFileService uploadFileService;
 
+    // @PostMapping("/upload")
+    // public void uploadCSV(@RequestParam("file") MultipartFile file ) throws
+    // Exception{
+    // uploadFileService.importCSV(file.getInputStream());
+    // }
+
     @PostMapping("/upload")
-    public void uploadCSV(@RequestParam("file") MultipartFile file ) throws Exception{
+    public ResponseEntity<String> uploadCSV(@RequestParam("file") MultipartFile file) throws Exception {
         uploadFileService.importCSV(file.getInputStream());
+        return ResponseEntity.ok().body("File " + file.getOriginalFilename() + " file uploaded successfully");
     }
 
 }
-
